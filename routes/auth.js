6f78936e27a43bfaac019fc3f540f9eaff3c932a
habibi <asurax1983@gmail.com>
2025-11-01 21:53:23 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { authenticateToken, requireAdmin, requireStudent } from "../middleware/auth.js";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
