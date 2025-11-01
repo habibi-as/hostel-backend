c7803055320c58c1bad1f548b6ff2479f0e88d8c
habibi <asurax1983@gmail.com>
2025-11-01 22:06:33 +0530

    Update events.js

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const Event = require('../models/Event');
-const User = require('../models/User');
+const User = require('../models/user');
 const { authenticateToken, requireAdmin, requireAnyRole } = require('../middleware/auth');
 
 const router = express.Router();
